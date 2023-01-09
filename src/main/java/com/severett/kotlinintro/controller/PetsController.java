984b9f04d70925d9f2ff0418e2a31599c8ce6cc4
Severn Everett <severn.everett@gmail.com>
2023-01-09 14:45:41 +0100

    Added endpoint to save pets

--- src/main/java/com/severett/kotlinintro/controller/PetsController.java
+++ src/main/java/com/severett/kotlinintro/controller/PetsController.java
@@ -1,6 +1,7 @@
 package com.severett.kotlinintro.controller;
 
 import com.severett.kotlinintro.exception.EntityNotFoundException;
+import com.severett.kotlinintro.exception.InternalException;
 import com.severett.kotlinintro.model.Pet;
 import com.severett.kotlinintro.model.PetType;
 import com.severett.kotlinintro.service.PetService;
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +36,11 @@ public class PetsController {
     @GetMapping()
     public Map<String, List<? extends Pet>> getAll() {
         return petService.getAll();
+    }
+
+    @PostMapping("/{type}")
+    public void create(@PathVariable PetType type, @RequestBody Map<String, Object> data) throws InternalException {
+        petService.create(type, data);
     }
 
     @DeleteMapping("/{type}/{id}")
--- src/main/java/com/severett/kotlinintro/service/PetService.java
+++ src/main/java/com/severett/kotlinintro/service/PetService.java
@@ -1,6 +1,10 @@
 package com.severett.kotlinintro.service;
 
 import com.severett.kotlinintro.exception.EntityNotFoundException;
+import com.severett.kotlinintro.exception.InternalException;
+import com.severett.kotlinintro.model.Cat;
+import com.severett.kotlinintro.model.Dog;
+import com.severett.kotlinintro.model.Horse;
 import com.severett.kotlinintro.model.Pet;
 import com.severett.kotlinintro.model.PetType;
 import com.severett.kotlinintro.repo.PetRepo;
@@ -13,6 +17,11 @@ import java.util.Map;
 @Service
 @AllArgsConstructor
 public class PetService {
+    private static final String NAME_FIELD = "name";
+    private static final String CLAW_LENGTH_FIELD = "clawLength";
+    private static final String TAIL_LENGTH_FIELD = "tailLength";
+    private static final String HOOF_WIDTH_FIELD = "hoofWidth";
+
     private final PetRepo petRepo;
 
     public Pet get(PetType type, int id) throws EntityNotFoundException {
@@ -21,6 +30,15 @@ public class PetService {
 
     public Map<String, List<? extends Pet>> getAll() {
         return petRepo.getAll();
+    }
+
+    public void create(PetType type, Map<String, Object> data) throws InternalException {
+        Pet petToCreate = switch (type) {
+            case cat -> new Cat(0, (String) data.get(NAME_FIELD), (Double) data.get(CLAW_LENGTH_FIELD));
+            case dog -> new Dog(0, (String) data.get(NAME_FIELD), (Double) data.get(TAIL_LENGTH_FIELD));
+            case horse -> new Horse(0, (String) data.get(NAME_FIELD), (Double) data.get(HOOF_WIDTH_FIELD));
+        };
+        petRepo.save(petToCreate);
     }
 
     public void delete(PetType type, int id) {
